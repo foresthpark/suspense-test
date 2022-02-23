4c720626c5dd0eeb96fb5f2db64531f4643d3485
Forest Park <foresthpark@gmail.com>
2022-02-22 19:35:49 -0700

    don

---
+++ components/TestComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TestComponent() {
+  return (
+    <div className="p-6 aspect-square bg-slate-400 flex justify-center items-center">
+      This is the component you were looking for
+    </div>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    concurrentFeatures: true,
+  },
+  images: {
+    domains: ["i.imgur.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
